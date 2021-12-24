7388a14707c8449e703b943d4de91e4b18f168b9
Endermens Night <75437783+Endermens@users.noreply.github.com>
2021-12-24 18:25:47 +0300

    Add files via upload

--- ex1.py
+++ ex1.py
@@ -1,11 +1,67 @@
-print("Введите два 3-х значныхчисла ")
+# Пользователь вводит данные о количестве предприятий, их наименования и прибыль за 4 квартала (т.е. 4 отдельных числа) для каждого предприятия.
+# Программа должна определить среднюю прибыль (за год для всех предприятий) и вывести наименования предприятий,
+# чья прибыль выше среднего и отдельно вывести наименования предприятий, чья прибыль ниже среднего.
 
-firts_number = int(input('\nпервое трехзнаачноечисло: '))
-second_number = int(input('второе трехзнаачноечисло: '))
+## дефолд вариант
+# print("укажите количество предприятий")
+#
+# c_factory = int(input("количество: "))
+# factory_name = []
+# profit = []
+# for i in range(1, (c_factory + 1)):
+#     factory_name.append(input(f"\nназвание {i}-го предприятия:\t"))
+#     factory_profit = int(input(f"ведите прибыль за 4-е квартала {i}-го завода: "))
+#     profit.append(factory_profit)
+# year_profit = sum(profit)//len(profit)
+# print(f"средняя прибыль предприятий за год состовляет: {year_profit} $\n")
+#
+# more = []
+# less = []
+# for i in range(c_factory):
+#     if profit[i] >= year_profit:
+#         more.append([f"{i+1}-е предприятие",profit[i]])
+#     else:
+#         less.append([f"{i+1}-е предприятие", profit[i]])
+#
+# print("предприятия чьих доход выше среднего")
+# if len(more) != 0:
+#     for i in range(len(more)):
+#         print(*more[i], sep = ': ')
+# else:
+#     print("...")
+# print('_' * 60, '\n')
+# print("предприятия чьих доход ниже среднего")
+# if len(less) != 0:
+#     for i in range(len(less)):
+#         print(*less[i], sep = ': ')
+# else:
+#     print("...")
 
-while firts_number < 100 or second_number < 100 or firts_number > 999 or second_number > 999:
-    print('попробуйте еще  ввести числа раз')
-    firts_number = int(input('\nпервое трехзнаачноечисло: '))
-    second_number = int(input('второе трехзнаачноечисло: '))
 
-print(firts_number * second_number)
+## вариант с приминением collections
+from collections import namedtuple
+from statistics import mean
+
+New_Company = namedtuple('New_Company', 'name profit_list avg')
+
+lst = []
+for i in range(int(input('Введите количество компани: '))):
+    arg = input('Введите в строку имя и поквартальную прибыль через пробел:\n').split()
+    lst.append(New_Company(arg[0], arg[1:], mean(map(int, arg[1:3]))))
+
+avg = mean([i.avg for i in lst])
+
+for i in lst:
+    print(f'Компания: {i.name} \t\tСредняя прибыль за год: {i.avg}')
+
+print('_' * 70, '\n')
+
+print('Компании с прибылью меньше средней:')
+for i in lst:
+    if i.avg < avg:
+        print(i.name)
+
+print('Компании с прибылью больше средней:')
+for i in lst:
+    if i.avg > avg:
+        print(i.name)
--- ex2.py
+++ ex2.py
@@ -1,3 +1,37 @@
-print(5 & 6) #and
-print( 5 | 6) #or
-print( 5 ^ 6) #-and
+from collections import defaultdict
+from collections import deque
+
+
+def my_dex(string):
+    dex = 0
+    num = deque(string)
+    num.reverse()
+    for i in range(len(num)):
+        dex += table[num[i]] * 16 ** i
+    return dex
+
+
+def my_hex(numb):
+    num = deque()
+    while numb > 0:
+        d = numb % 16
+        for i in table:
+            if table[i] == d:
+                num.append(i)
+        numb //= 16
+    num.reverse()
+    return list(num)
+
+
+signs = '0123456789ABCDEF'
+table = defaultdict(int)
+counter = 0
+for k in signs:
+    table[k] += counter
+    counter += 1
+
+num_1 = my_dex(input('Введите первое 16-е число: ').upper())
+num_2 = my_dex(input('Введите второе 16-е число:\n ').upper())
+
+print(f'Сумма чисел: {my_hex(num_1 + num_2)}')
+print(f'Произведение чисел: {my_hex(num_1 * num_2)}')
